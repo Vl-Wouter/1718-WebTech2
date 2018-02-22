7718ac6e5c3c157e0d3ca24423a557bbf60d05f8
Vl-Wouter <36659001+Vl-Wouter@users.noreply.github.com>
2018-02-22 10:58:45 +0100

    First JS Test
    First try

--- Week2/js/oefening1.js
+++ Week2/js/oefening1.js
@@ -1,3 +1,14 @@
+let number = document.getElementById("thenumber");
+let OddEven;
+if(number % 2 == 0) {
+    OddEven = "Even";
+}
+
+else {
+    OddEven = "Odd";
+}
+
 function result() {
-    let number = getElementById
+    const res = "The number you wrote is " + OddEven;
+    document.getElementById('oddOrEven').innerHTML = res;
 }
