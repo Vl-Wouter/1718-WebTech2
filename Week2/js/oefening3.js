edc7c95080911252cdc0830dd8761fa3784286a0
Vl-Wouter <36659001+Vl-Wouter@users.noreply.github.com>
2018-02-22 11:41:37 +0100

    Added Oefening 3
    Ez

---
+++ Week2/js/oefening3.js
@@ -0,0 +1,9 @@
+let words = ["help ", "me ", "ffs"];
+
+for(i == 0; i < words.length; i++) {
+    console.log(words[i]);
+}
+
+let together = words[0] + words[1] + words[2];
+
+console.log(together);
