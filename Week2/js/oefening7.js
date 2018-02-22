bce2947516c5abc158313d8708125f9e82f04b01
Vl-Wouter <36659001+Vl-Wouter@users.noreply.github.com>
2018-02-22 13:54:44 +0100

    Copyright info

--- Week2/js/oefening7.js
+++ Week2/js/oefening7.js
@@ -1,3 +1,10 @@
+/**********************************
+ * @author      Wouter Vlaeyen
+ * @created     22/02/2018
+ * @modified    22/02/2018
+ * @copyright   Copyright © 2016-2017 Artevelde University College Ghent
+ * @function    Week 2
+ **********************************/
 let calculate = function() {
     const begin = parseInt(document.getElementById('begin').value);
     const eind = parseInt(document.getElementById('eind').value);
