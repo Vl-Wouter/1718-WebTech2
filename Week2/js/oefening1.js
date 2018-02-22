56d9a80e2064261e24050320da2521139fb5bc2d
Vl-Wouter <36659001+Vl-Wouter@users.noreply.github.com>
2018-02-22 11:27:33 +0100

    ''?

--- Week2/js/oefening1.js
+++ Week2/js/oefening1.js
@@ -1,6 +1,6 @@
 
 function result() {
-    let number = document.getElementById("thenumber").value;
+    let number = document.getElementById('thenumber').value;
     number = parseInt(number);
     console.log("Number = " + number);
     let oddEven;
