6a88a14e874103c9ad71102c9c03c7875126c9f9
Vl-Wouter <36659001+Vl-Wouter@users.noreply.github.com>
2018-02-22 11:47:25 +0100

    Edited Oefening 3
    Automated adding together of array

--- Week2/js/oefening3.js
+++ Week2/js/oefening3.js
@@ -1,9 +1,14 @@
 let words = ["help ", "me ", "ffs"];
 
-for(i == 0; i < words.length; i++) {
+for(let i = 0; i < words.length; i++) {
     console.log(words[i]);
 }
 
-let together = words[0] + words[1] + words[2];
+//let together = words[0] + words[1] + words[2];
+
+let together = words[0];
+for(let i = 1; i < words.length; i++) {
+    together += words[i];
+}
 
 console.log(together);
