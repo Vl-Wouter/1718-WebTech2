c1f9e2c090047cc086d0c1f03ca24927f116e307
Vl-Wouter <36659001+Vl-Wouter@users.noreply.github.com>
2018-02-22 11:09:53 +0100

    Fixed now?

--- Week2/js/oefening1.js
+++ Week2/js/oefening1.js
@@ -1,17 +1,17 @@
-let number = parseInt(document.getElementById("thenumber"));
+let number = parseInt(document.getElementById("thenumber").value);
 console.log("Number = " + number);
 let oddEven;
 if(number % 2 == 0) {
-    oddEven = "Even";
+    oddEven = "oven";
 }
 
 else {
-    oddEven = "Odd";
+    oddEven = "odd";
 }
 
 console.log("Test = " + oddEven);
 
 function result() {
-    const res = "The number you wrote is " + OddEven;
+    const res = "The number you wrote is " + oddEven + ".";
     document.getElementById('oddOrEven').innerHTML = res;
 }
