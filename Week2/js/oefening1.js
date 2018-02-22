e183dd3542c26966774ec783ecc42c50921a0d66
Vl-Wouter <36659001+Vl-Wouter@users.noreply.github.com>
2018-02-22 11:14:32 +0100

    Put everything in function

--- Week2/js/oefening1.js
+++ Week2/js/oefening1.js
@@ -1,18 +1,19 @@
-let number = document.getElementById("thenumber").value;
-number = parseInt(number);
-console.log("Number = " + number);
-let oddEven;
-if(number % 2 === 0) {
-    oddEven = "even";
-}
-
-else {
-    oddEven = "odd";
-}
-
-console.log("Test = " + oddEven);
 
 function result() {
+    let number = document.getElementById("thenumber").value;
+    number = parseInt(number);
+    console.log("Number = " + number);
+    let oddEven;
+    if(number % 2 === 0) {
+        oddEven = "even";
+    }
+    
+    else {
+        oddEven = "odd";
+    }
+    
+    console.log("Test = " + oddEven);
+    
     const res = "The number you wrote is " + oddEven + ".";
     document.getElementById('oddOrEven').innerHTML = res;
 }
