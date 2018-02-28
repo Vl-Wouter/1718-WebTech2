35c542bac59779898bbc5f6ea3bb27a6b2804ecc
Vl-Wouter <36659001+Vl-Wouter@users.noreply.github.com>
2018-02-28 22:12:55 +0100

    Test
    Test using Atom as main editor

---
+++ Week3/test.js
@@ -0,0 +1,10 @@
+/**
+ * @Author: Wouter Vlaeyen
+ * @Date:   2018-02-28T22:10:36+01:00
+ * @Email:  woutvlae@student.arteveldehs.be
+ * @Filename: test.js
+ * @Last modified by:   Wouter Vlaeyen
+ * @Last modified time: 2018-02-28T22:10:37+01:00
+ * @License: Apache 2.0
+ * @Copyright: Copyright © 2017-2018 Artevelde University College Ghent
+ */
