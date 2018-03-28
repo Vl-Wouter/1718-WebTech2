664f52686ef77ed0887ddb37481bdb4602a560b0
Wouter Vlaeyen <36659001+Vl-Wouter@users.noreply.github.com>
2018-03-28 21:34:07 +0200

    Added Week7

---
+++ Week7/js/main.js
@@ -0,0 +1,68 @@
+/**
+ * @Author: Wouter Vlaeyen
+ * @Date:   28-03-2018
+ * @Email:  woutvlae@student.arteveldehs.be
+ * @Filename: main.js
+ * @Last modified by:   Wouter Vlaeyen
+ * @Last modified time: 28-03-2018
+ * @Copyright: 2017-2018 Artevelde University College Ghent
+ */
+// Get form inputs
+let firstInput = document.getElementById('firstName');
+let lastInput = document.getElementById('lastName');
+
+
+// get JSON via url
+const getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+};
+
+// get JSON for this exercise
+
+//Get joke
+let createJoke = function() {
+  let firstName = firstInput.value;
+  let lastName = lastInput.value;
+  let url = 'http://api.icndb.com/jokes/random?firstName=' + firstName + '&lastName=' + lastName;
+  console.log(url);
+  getJSON(url, function(error, data) {
+    if(error) {
+      alert('There has been an error getting the JSON data, please check the code or try again.');
+      return false;
+    }
+
+    let joke = data;
+    let jokelist = document.getElementById('jokes');
+
+    let jokecontainer = document.createElement('li');
+    jokecontainer.classList.add('jokeItem');
+    let joketxtcont = document.createElement('p');
+    joketxtcont.classList.add('joketxt')
+    joketxtcont.innerHTML = joke.value.joke;
+    let jokeidcont = document.createElement('p');
+    jokeidcont.classList.add('jokeid')
+    let jokeid = document.createTextNode('joke ID: ' + joke.value.id);
+    jokeidcont.appendChild(jokeid);
+    jokecontainer.appendChild(joketxtcont);
+    jokecontainer.appendChild(jokeidcont);
+    console.log(joketxtcont.innerHTML);
+    jokelist.appendChild(jokecontainer);
+  });
+  firstInput.value = firstName;
+  lastInput.value = lastName;
+};
+
+
+//Event Listener
+document.getElementById('btnGen').addEventListener('click', createJoke);
